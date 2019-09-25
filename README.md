495ad1ae7f7061ecbe376ec2ad51c84a0ad337e9
ricardo.fahham <ricardo.fahham@Ricardo-Fahham-2.local>
2019-09-25 15:51:57 -0300

    atualizando documentação

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Status_Code
+
+$ sudo -H pip install requests
+$ sudo -H pip install urllib3
+$ sudo -H pip install certifi
+
+OU
+
+pip install -r requirements.txt
+
+
+Docs
+
+instalar o pip => sudo easy_install pip
+instalar o request sudo pip install requests => http://requests-docs-pt.readthedocs.io/pt_BR/latest/user/quickstart.html
+http://docs.python-requests.org/en/master/user/quickstart/#make-a-request
+http://docs.python-requests.org/en/latest/user/advanced/#ssl-cert-verification
+https://docs.python.org/2/howto/urllib2.html
+https://urllib3.readthedocs.io/en/latest/user-guide.html#ssl
+
+https://www.tutorialspoint.com/python/python_if_else.htm
+https://forum.gamemods.com.br/threads/python-07-estrutura-de-repeticao.36655/
+https://stackoverflow.com/questions/27981545/suppress-insecurerequestwarning-unverified-https-request-is-being-made-in-pytho
--- get200.py
+++ get200.py
@@ -1,16 +1,6 @@
 # !/usr/bin/env python
 # -- coding: utf-8 --
 
-
-# $ sudo -H pip install requests
-# $ sudo -H pip install urllib3
-# $ sudo -H pip install certifi
-
-# OU
-
-# pip install -r requirements.txt
-
-
 import requests
 import urllib3
 import certifi
@@ -76,18 +66,3 @@ print
 arq.close()
 arq_200.close()
 arq_404.close()
-
-
-
-# Docs
-
-# instalar o pip => sudo easy_install pip
-# instalar o request sudo pip install requests => http://requests-docs-pt.readthedocs.io/pt_BR/latest/user/quickstart.html
-# http://docs.python-requests.org/en/master/user/quickstart/#make-a-request
-# http://docs.python-requests.org/en/latest/user/advanced/#ssl-cert-verification
-# https://docs.python.org/2/howto/urllib2.html
-# https://urllib3.readthedocs.io/en/latest/user-guide.html#ssl
-
-# https://www.tutorialspoint.com/python/python_if_else.htm
-# https://forum.gamemods.com.br/threads/python-07-estrutura-de-repeticao.36655/
-# https://stackoverflow.com/questions/27981545/suppress-insecurerequestwarning-unverified-https-request-is-being-made-in-pytho
