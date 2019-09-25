bd0ea508e7a40d53292b5dce138ff48671ac90c9
ricardo.fahham <ricardo.fahham@Ricardo-Fahham-2.local>
2019-09-25 15:42:13 -0300

    subindo código atualizado

--- get200.py
+++ get200.py
@@ -1,6 +1,7 @@
 # !/usr/bin/env python
 # -- coding: utf-8 --
 
+
 # $ sudo -H pip install requests
 # $ sudo -H pip install urllib3
 # $ sudo -H pip install certifi
@@ -9,22 +10,24 @@
 
 # pip install -r requirements.txt
 
+
 import requests
 import urllib3
 import certifi
+import time
+import datetime
 
-arq = open('nome do arquivo', 'r') # arquivo com a lista de urls
+arq = open('lista.csv', 'r')
 arq_200 = open('200.csv', 'w')
-arq_404 = open('401.csv', 'w')
 arq_404 = open('404.csv', 'w')
-arq_500 = open('500.csv', 'w')
+arq_405 = open('405.csv', 'w')
+arq_500 = open('405.csv', 'w')
 
 cont_200 = 0
-cont_401 = 0
 cont_404 = 0
+cont_405 = 0
 cont_500 = 0
 
- 
 urllib3.disable_warnings()
 
 while True:
@@ -36,37 +39,44 @@ while True:
     r = requests.get(url, verify=False)
     
     if (r.status_code == 200):
-        print('Status Code 200 ')
+        print('\033[32mStatus Code 200\033[m')
         arq_200.write(linha)
         cont_200 += 1
+
+    if (r.status_code == 405):
+        print('\033[31mStatus Code 405\033[m')
+        arq_405.write(linha)
+        cont_405 += 1
+
     if (r.status_code == 404):
-        print('Status Code 404 ')
+        print('\033[31mStatus Code 404\033[m')
         arq_404.write(linha)
         cont_404 += 1
-    if (r.status_code == 500):
-        print('Status Code 500 ')
+
+    elif (r.status_code == 500):
+        print('\033[31mStatus Code 500\033[m')
         arq_500.write(linha)
         cont_500 += 1
 
 print
 print '---------------------------'
 print
-print 'Total de urls:', (cont_200 + cont_404 + cont_500) 
-print    
-print 'Páginas Status Code 200:', (cont_200)
+print "Data: " , datetime.datetime.now().strftime("%d/%m/%y"), "- Hora: " , datetime.datetime.now().strftime("%H:%M")
+print       
+print 'Páginas com Status Code 200:', (cont_200)
 print
-print 'Páginas Status Code 404:', (cont_404)
+print 'Páginas com Status Code 404:', (cont_404)
 print
-print 'Páginas Status Code 500:', (cont_500)
+print 'Páginas com Status Code 405:', (cont_405)
 print
 print '---------------------------'
 print
 
+
 arq.close()
 arq_200.close()
-arq_401.close()
 arq_404.close()
-arq_500.close()
+
 
 
 # Docs
@@ -81,4 +91,3 @@ arq_500.close()
 # https://www.tutorialspoint.com/python/python_if_else.htm
 # https://forum.gamemods.com.br/threads/python-07-estrutura-de-repeticao.36655/
 # https://stackoverflow.com/questions/27981545/suppress-insecurerequestwarning-unverified-https-request-is-being-made-in-pytho
-
--- teste_de_leitura_de_arquivo.py
+++
@@ -1,7 +0,0 @@
-# !/usr/bin/env python
-# -- coding: utf-8 --
-
-arquivo = open('lista_get.csv', 'r')
-print arquivo
-
-arquivo.close()
